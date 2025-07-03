68cd4685b49bf839b043a98adf26b46a3cff053a
minggo <nudtzxm@gmail.com>
2025-07-03 19:11:21 +1200

    remove unneeded contents

--- server/app/data/__init__.py
+++
@@ -1,7 +0,0 @@
-import json
-from importlib.resources import files
-
-def load_words_from_file():
-    # returns a Python list of dicts
-    data = files("app.data").joinpath("words.json").read_text(encoding="utf-8")
-    return json.loads(data)
